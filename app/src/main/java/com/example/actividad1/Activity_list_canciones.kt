10860b40a95a57520cf25dc1346770db2b67ef83
VEALASCO <velascoale83@gmail.com>
2024-04-18 09:45:41 -0600

    Actualizanción

--- app/src/main/java/com/example/actividad1/Activity_list_canciones.kt
+++ app/src/main/java/com/example/actividad1/Activity_list_canciones.kt
@@ -1,20 +1,32 @@
 package com.example.actividad1
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.ListView
 
 class Activity_list_canciones : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_list_canciones)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val lv_canciones = findViewById<ListView>(R.id.lv_canciones)
+        val list_canciones: ArrayList<String> = ArrayList()
+
+        val canciones = CancionesHelper(this,"cancionesdb", null, 1)
+        val bd = canciones.writableDatabase
+        val consulta = bd.rawQuery("select id, titulo, cantante from cancion", null)
+        while (consulta.moveToNext()){
+            val id = consulta.getString(0)
+            val titulo = consulta.getString(1)
+            val cantante = consulta.getString(2)
+            list_canciones.add(id+ "- "+titulo+"- "+cantante)
         }
+        val adaptador = ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,list_canciones)
+        lv_canciones.adapter = adaptador
+        bd.close()
     }
 }
---
+++ app/src/main/java/com/example/actividad1/Pelicula.kt
@@ -0,0 +1,4 @@
+package com.example.actividad1
+
+class Pelicula(val tituulo: String, val  genero: String) {
+}
